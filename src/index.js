7c5bea3953ae6139e6365d8e81ccff6f9ee7be7b
Mário César Silva <mcgato33@hotmail.com>
2020-09-27 12:50:12 -0100

    create src

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return(
+    <View style={styles.container} >
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    backgroundColor: '#7159c1',
+  },
+});   
+
+export default App; 
