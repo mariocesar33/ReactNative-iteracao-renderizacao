7383e6a76427672f6e2df153d755cb85852d34d7
Mário César Silva <mcgato33@hotmail.com>
2020-09-28 16:34:38 -0100

    linking of react-native-vector-icons

--- src/index.js
+++ src/index.js
@@ -1,29 +1,56 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Button, Icon } from 'react-native-elements';
 
 import UserList from './views/UserList';
 import UserForm from './views/UserForm';
+
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return(
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="UserList">
+      <Stack.Navigator 
+        initialRouteName="UserList"
+        screenOptions={screenOptions}
+      >
         <Stack.Screen
           name="UserList"
           component={UserList}
+          options={() => {
+            return {
+              title: "Lista de Utilizadores",
+              headerRight: () => (
+                <Button 
+                  type="clear"
+                  icon={<Icon name="add" size={25} color="white" />}
+                />
+              )
+            }
+          }}
         />
         <Stack.Screen
           name="UserForm"
           component={UserForm}
+          options={{
+            title: "Formulário de Utlilizadores"
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-   
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#f4511e',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+}
 
 export default App; 
